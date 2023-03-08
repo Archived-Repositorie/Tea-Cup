50566da34702128e310eaa3491801ff76a2ce112
JustFoxx <justfoxxo@outlook.com>
2023-03-08 17:12:13 +0100

    Made it better for java users

--- src/main/kotlin/io/github/justfoxx/teacup/v1/event/Events.kt
+++ src/main/kotlin/io/github/justfoxx/teacup/v1/event/Events.kt
@@ -5,28 +5,42 @@ import io.github.justfoxx.teacup.v1.registry.SetRegistry
 import net.minecraft.entity.Entity
 
 /**
- * A class that holds all the events
+ * A singleton object that contains event keys for all supported events.
  */
 object Events {
+
     /**
-     * An event that is called when a player uses an item
+     * Event key for when a player starts using an item.
+     * This event is triggered when the player starts the action of using an item.
      */
-    val ON_ITEM_USING: EventKey<(OnItemUseData) -> Unit, OnItemUseData> =
+    @JvmStatic val ON_ITEM_USING: EventKey<(OnItemUseData) -> Unit, OnItemUseData> =
         EventKey(SetRegistry()) { data, info ->
             EventFunctions.onItemUse(data, info!!)
         }
 
-    val ON_ITEM_USED: EventKey<(OnItemUseData) -> Unit, OnItemUseData> =
+    /**
+     * Event key for when a player finishes using an item.
+     * This event is triggered when the player finishes the action of using an item.
+     */
+    @JvmStatic val ON_ITEM_USED: EventKey<(OnItemUseData) -> Unit, OnItemUseData> =
         EventKey(SetRegistry()) { data, info ->
             EventFunctions.onItemUse(data, info!!)
         }
 
-    val ON_ENTITY_TICKING: EventKey<(Entity) -> Unit, Entity> =
+    /**
+     * Event key for when an entity starts ticking.
+     * This event is triggered when an entity starts updating its state.
+     */
+    @JvmStatic val ON_ENTITY_TICKING: EventKey<(Entity) -> Unit, Entity> =
         EventKey(SetRegistry()) { data, info ->
             EventFunctions.onEntityTick(data, info!!)
         }
 
-    val ON_ENTITY_TICKED: EventKey<(Entity) -> Unit, Entity> =
+    /**
+     * Event key for when an entity finishes ticking.
+     * This event is triggered when an entity has finished updating its state.
+     */
+    @JvmStatic val ON_ENTITY_TICKED: EventKey<(Entity) -> Unit, Entity> =
         EventKey(SetRegistry()) { data, info ->
             EventFunctions.onEntityTick(data, info!!)
         }
