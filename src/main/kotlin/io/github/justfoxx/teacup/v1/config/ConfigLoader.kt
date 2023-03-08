24208f1c0908a31fa04eb7ebf9ed53158a3d5bcb
JustFoxx <justfoxxo@outlook.com>
2023-03-08 13:10:19 +0100

    Added documentation

--- src/main/kotlin/io/github/justfoxx/teacup/v1/config/ConfigLoader.kt
+++ src/main/kotlin/io/github/justfoxx/teacup/v1/config/ConfigLoader.kt
@@ -8,7 +8,14 @@ import java.nio.file.Path
 import java.util.Optional
 
 object ConfigLoader {
-    fun <T> loadConfig(path: Path, configClass: Class<T>? = null): Pair<Optional<T & Any>, JsonObject> {
+    /**
+     * Loads a configuration file from the given path, creating it if it doesn't exist.
+     *
+     * @param path The path to the configuration file.
+     * @param configClass The class of the configuration object. If null, returns the raw JSON object.
+     * @return A Pair of an Optional of the loaded configuration object, and the raw JSON object.
+     */
+    fun <T> loadOrCreateConfigFile(path: Path, configClass: Class<T>? = null): Pair<Optional<T & Any>, JsonObject> {
         val configPath = FabricLoader.getInstance().configDir.resolve(path)
 
         val configObject: JsonObject = if (Files.notExists(configPath)) {
@@ -21,7 +28,7 @@ object ConfigLoader {
             JsonHelper.readJsonObject(configPath)
         }
 
-        return if(configClass != null)
+        return if (configClass != null)
             Pair(Optional.ofNullable(JsonHelper.convert(configObject, configClass)), configObject)
         else
             Pair(Optional.empty(), configObject)
