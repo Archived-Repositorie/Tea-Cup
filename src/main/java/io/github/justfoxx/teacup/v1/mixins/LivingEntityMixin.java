aa8b427c26803c0cba4c9318420086a04d792de6
JustFoxx <justfoxxo@outlook.com>
2023-03-12 15:33:40 +0100

    Fix

--- src/main/java/io/github/justfoxx/teacup/v1/mixins/LivingEntityMixin.java
+++ src/main/java/io/github/justfoxx/teacup/v1/mixins/LivingEntityMixin.java
@@ -1,6 +1,7 @@
 package io.github.justfoxx.teacup.v1.mixins;
 
 import io.github.justfoxx.teacup.v1.event.Events;
+import io.github.justfoxx.teacup.v1.event.SingletonData;
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,11 +12,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class LivingEntityMixin {
     @Inject(method = "onDeath", at = @At("HEAD"))
     private void onDying(CallbackInfo ci) {
-        Events.getON_ENTITY_DYING().invoker().invoke((LivingEntity) (Object) this);
+        Events.getON_ENTITY_DYING().invoker().invoke(new SingletonData<>((LivingEntity) (Object) this, ci));
     }
 
     @Inject(method = "onDeath", at = @At("RETURN"))
     private void onDied(CallbackInfo ci) {
-        Events.getON_ENTITY_DIED().invoker().invoke((LivingEntity) (Object) this);
+        Events.getON_ENTITY_DIED().invoker().invoke(new SingletonData<>((LivingEntity) (Object) this, ci));
     }
 }
--- src/main/java/io/github/justfoxx/teacup/v1/mixins/MinecraftServerMixin.java
+++ src/main/java/io/github/justfoxx/teacup/v1/mixins/MinecraftServerMixin.java
@@ -1,6 +1,7 @@
 package io.github.justfoxx.teacup.v1.mixins;
 
 import io.github.justfoxx.teacup.v1.event.Events;
+import io.github.justfoxx.teacup.v1.event.SingletonData;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,31 +12,31 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MinecraftServerMixin {
     @Inject(method = "runServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;setupServer()Z"))
     private void beforeServerStarting(CallbackInfo ci) {
-        Events.getON_SERVER_STARTING().invoker().invoke((MinecraftServer) (Object) this);
+        Events.getON_SERVER_STARTING().invoker().invoke(new SingletonData<>((MinecraftServer) (Object) this, ci));
     }
 
     @Inject(method = "runServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;createMetadata()Lnet/minecraft/server/ServerMetadata;", ordinal = 0))
     private void afterServerStarted(CallbackInfo ci) {
-        Events.getON_SERVER_STARTED().invoker().invoke((MinecraftServer) (Object) this);
+        Events.getON_SERVER_STARTED().invoker().invoke(new SingletonData<>((MinecraftServer) (Object) this, ci));
     }
 
     @Inject(method = "shutdown", at = @At("HEAD"))
     private void beforeServerStopping(CallbackInfo ci) {
-        Events.getON_SERVER_STOPPING().invoker().invoke((MinecraftServer) (Object) this);
+        Events.getON_SERVER_STOPPING().invoker().invoke(new SingletonData<>((MinecraftServer) (Object) this, ci));
     }
 
     @Inject(method = "shutdown", at = @At("RETURN"))
     private void afterServerStopped(CallbackInfo ci) {
-        Events.getON_SERVER_STOPPED().invoker().invoke((MinecraftServer) (Object) this);
+        Events.getON_SERVER_STOPPED().invoker().invoke(new SingletonData<>((MinecraftServer) (Object) this, ci));
     }
 
     @Inject(method = "tick", at = @At("HEAD"))
     private void onTicking(CallbackInfo ci) {
-        Events.getON_SERVER_TICKING().invoker().invoke((MinecraftServer) (Object) this);
+        Events.getON_SERVER_TICKING().invoker().invoke(new SingletonData<>((MinecraftServer) (Object) this, ci));
     }
 
     @Inject(method = "tick", at = @At("RETURN"))
     private void onTicked(CallbackInfo ci) {
-        Events.getON_SERVER_TICKED().invoker().invoke((MinecraftServer) (Object) this);
+        Events.getON_SERVER_TICKED().invoker().invoke(new SingletonData<>((MinecraftServer) (Object) this, ci));
     }
 }
